45205ae2b0af6657054c5684c6fdba37c622ad18
Hajjouz <18026754+Hajjouz@users.noreply.github.com>
2025-10-08 11:54:30 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/ethereumnode/main.go
@@ -0,0 +1,23 @@
+// cmd/ethereumnode/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "ethereumnode/internal/ethereumnode"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := ethereumnode.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
